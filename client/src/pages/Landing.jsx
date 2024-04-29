107829cc7016a7c129d684e6d7ac8f7b5b117db6
hridyauk <133180993+hridyauk@users.noreply.github.com>
2024-04-29 00:01:32 -0700

    added landing page content and buttons

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,36 @@
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            praesentium saepe quisquam suscipit voluptates placeat iste vitae
+            est quibusdam natus, voluptatum cupiditate, assumenda perspiciatis
+            repudiandae neque ad porro, aliquid pariatur.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
