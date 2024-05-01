1f7ccfb7d04e429d2b66f9d73edf9a0744dac4e7
hridyauk <133180993+hridyauk@users.noreply.github.com>
2024-04-30 17:43:54 -0700

    base structure added for sidebar and navbar

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,5 +1,22 @@
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { BigSidebar, NavBar, SmallSidebar } from "../components";
+
 const DashboardLayout = () => {
-  return <h1>DashboardLayout</h1>;
+  return (
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <NavBar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+  );
 };
 
 export default DashboardLayout;
