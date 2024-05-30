4308a128770e0850381f9c7867e3f0258c2ef330
hridyauk <133180993+hridyauk@users.noreply.github.com>
2024-05-29 18:06:19 -0700

    create job

--- server.js
+++ server.js
@@ -26,8 +26,22 @@ app.post("/", (req, res) => {
   res.json({ message: "data received", data: req.body });
 });
 
+// fetches all job detail
 app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
+});
+
+// create job
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    res.status(400).json({ msg: "Please provide company and position" });
+    return;
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
 });
 
 const port = process.env.PORT || 5100;
