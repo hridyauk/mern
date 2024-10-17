ed707314f09202b6fc3a346b7ef643ee88af5f14
hridyauk <133180993+hridyauk@users.noreply.github.com>
2024-10-16 21:42:44 -0700

    added validation

---
+++ middleware/validationMiddleware.js
@@ -0,0 +1,45 @@
+import { body, param, validationResult } from "express-validator";
+import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
+import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
+import mongoose from "mongoose";
+import Job from "../models/JobModel.js";
+
+const withValidationErrors = (validateValues) => {
+  return [
+    validateValues,
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map((error) => error.msg);
+        if (errorMessages[0].startsWith("no job")) {
+          throw new NotFoundError(errorMessages);
+        }
+        throw new BadRequestError(errorMessages);
+      }
+      next();
+    },
+  ];
+};
+
+export const validateJobInput = withValidationErrors([
+  body("company").notEmpty().withMessage("Company is required"),
+  body("position").notEmpty().withMessage("Position is required"),
+  body("jobLocation").notEmpty().withMessage("Job location is required"),
+  body("jobStatus")
+    .isIn(Object.values(JOB_STATUS))
+    .withMessage("Invalid status value"),
+  body("jobType")
+    .isIn(Object.values(JOB_TYPE))
+    .withMessage("Invalid type value"),
+]);
+
+export const validateIdParam = withValidationErrors([
+  param("id").custom(async (value) => {
+    const isValidId = mongoose.Types.ObjectId.isValid(value);
+    if (!isValidId) throw new BadRequestError("Invalid MongoDB Id");
+
+    const job = await Job.findById(value);
+
+    if (!job) throw new NotFoundError(`no job with id ${value}`);
+  }),
+]);
