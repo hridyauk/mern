3c9dcdff435ae015a512105ded3eb3fb1f2a11ce
hridyauk <133180993+hridyauk@users.noreply.github.com>
2025-12-18 17:11:11 -0800

    Recent changes

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { login, register } from "../controllers/authController.js";
+import { validateLoginInput, validateRegisterInput } from "../middleware/validationMiddleware.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateRegisterInput, register);
+router.post("/login", validateLoginInput, login);
 
 export default router;
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 // routers
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
 
 // middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 dotenv.config();
 const app = express();
@@ -16,7 +18,7 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -28,7 +30,7 @@ app.post("/", (req, res) => {
   res.json({ message: "data received", data: req.body });
 });
 
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
